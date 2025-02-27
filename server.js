9179db5d4580cc7a7ff4ac67d536bb56e90412a2
sebascadpi <sebascadpi@gmail.com>
2025-02-27 06:50:52 +0100

    feat: added upload csv endpoint to backend

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const multer = require('multer');
-const csvParser = require('csv-parser');
-const fs = require('fs');
-const cors = require('cors');
+const express = require("express");
+const multer = require("multer");
+const csvParser = require("csv-parser");
+const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
@@ -10,17 +10,49 @@ const port = 3000;
 // Enable CORS
 app.use(cors());
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
-app.post('/upload', upload.single('csvFile'), (request, response) => {
-    if (!request.file) return response.status(400).send('No file uploaded.');
+app.post("/upload", upload.single("csvFile"), (request, response) => {
+  if (!request.file) {
+    console.error("No file uploaded.");
+    return response.status(400).send("No file uploaded.");
+  }
 
-    const results = [];
-    fs.createReadStream(request.file.path)
-        .pipe(csvParser())
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-            fs.unlinkSync(request.file.path);
-            response.json(results);
-        })
-})
+  console.log("File uploaded successfully:", request.file);
+
+  const results = [];
+  fs.createReadStream(request.file.path)
+    .pipe(csvParser())
+    .on("data", (data) => {
+      console.log("Parsed data:", data);
+      results.push(data);
+    })
+    .on("error", (error) => {
+      console.error("Error while parsing CSV:", error);
+      response.status(500).send("Error while parsing CSV.");
+    })
+    .on("end", () => {
+      console.log("Finished parsing CSV. Results:", results);
+      fs.unlinkSync(request.file.path);
+
+      if (!results.length) return response.status(400).send("No data found.");
+      const headers = Object.keys(results[0])[0].split(";");
+      const rows = results.map((result) => Object.values(result)[0].split(";"));
+      const mappedRows = rows.map((row) => {
+        if (!row?.length) return row;
+        return (objectRow = row.reduce(
+          (acc, value, index) => ({
+            ...acc,
+            [headers[index]]: value,
+          }),
+          {}
+        ));
+      });
+
+      response.json({ headers, rows: mappedRows, length: results.length });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
